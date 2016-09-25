c6bee54ff6b05b5288ea0516e7911d8adb4b5ad6
D. Elion <d.elion@claritypartners.com>
2016-09-25 17:28:16 -0500

    stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,34 @@
 jQuery.noConflict();
 jQuery(function($) {
 	console.log('being read');
+
+
+	$(window).scroll(function() {
+        //var scroll = $(window).scrollTop();
+            var scroll = window.pageYOffset,
+            width = parseInt(jQuery(window).width()) >= 980;
+
+        if ((scroll >= 50) && (width)) {
+            $(".header-wrapper").addClass("small");
+            $("#block-menu-menu-menu-1, #block-menu-menu-menu-2").addClass("small");
+        } else {
+            $(".header-wrapper").removeClass("small");
+            $("#block-menu-menu-menu-1, #block-menu-menu-menu-2").removeClass("small");
+        }
+    });
+
+		$('a[href*=#]:not([href=#])').click(function() {
+			 if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				 var target = $(this.hash);
+				 target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				 if (target.length) {
+					 $('html,body').animate({
+						 scrollTop: target.offset().top
+					 }, 1000);
+					 return false;
+				 }
+			 }
+		 });
+
+
 });
